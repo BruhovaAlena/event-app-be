61c9ddff03fecc6d9fddf7ffe1eb22b860740e74
BruhovaAlena <bruhovaalena@gmail.com>
2022-11-10 16:51:15 +0100

    edit

--- index.ts
+++ index.ts
@@ -1,12 +1,14 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
-
+app.use(cors());
 app.get('/api', (req: Request, res: Response) => {
+  console.log('prisiel request');
   res.json({ users: ['userOne', 'userTwo', 'userThree'] });
 });
 
